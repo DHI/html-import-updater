0f6aab56a805a861ab012b95542c7a53e52d3423
Dan Mindru <mindrudan@gmail.com>
2017-09-15 09:03:38 +0200

    Add some test assets and test notes

---
+++ test/test.js
@@ -0,0 +1,7 @@
+// look for *.html
+// should work for js|html|css
+// href & src
+// allow module prefix
+// some paths can be nested; should handle relative paths
+// only ../ (and lower) should work; scan cwd and if files exist don't touch
+// exlude ../../js ; ../../css
