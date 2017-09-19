f5978daf36ba6a91722d5b69b4a78e1e13b1c07f
Dan Mindru <mindrudan@gmail.com>
2017-09-19 09:30:16 +0200

    Write program runtime using commander & inquirer

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 import inquirer from 'inquirer'
+import program from 'commander'
+import colors from 'colors'
+import path from 'path'
 import { reader, parser, replacer, outputer } from './lib'
 
-// TODO: pass params below through CLI
+let excludePaths = []
+let excludePatterns = []
+const collectExcludePaths = (value) => excludePaths = [...excludePaths, value]
+const collectExcludePatterns = (value) => excludePatterns = [...excludePatterns, value]
+
+program
+  .version('1.0.0')
+  .option('--cwd, --workingDir <workingDir>', 'Current working directory (where your files are).')
+  .option('--e, --excludePaths [excludePaths]', 'One or many paths (or globs) you want to exclude, i.e. `-e "prefix-*"`. Can be applied multiple times: `-e "one" -e "two"`.', collectExcludePaths)
+  .option('--o, --outputDir <outputDir>', 'The output directory.')
+  .option('--s, --search <search>', 'The string you want to search for, i.e. "../"')
+  .option('--r, --replace <replace>', 'The string you want to replace each occurence of the search string with.')
+  .option('--ep, --excludePatterns <excludePatterns>', 'One or many regular expressions that you want to exclude from each matched search item, i.e. `-ep "iron.*"`. Can be applied multiple times: `-ep "one" -ep "two"`.', collectExcludePatterns)
+  .parse(process.argv)
+
 async function runtime () {
-  const fileList = await reader({
-     workingDir: 'test-assets/examples' // hardcoded
-  })
+  const { workingDir, excludePaths, outputDir, search, replace, excludePatterns} = program
+
+  const fileList = await reader({ workingDir, excludePaths })
 
   const parseResults = await parser({
+    workingDir,
     fileList,
-    search: '../', // hardcoded
-    replace: '../lib/' // hardcoded
+    search,
+    replace,
+    excludePatterns
   })
 
   const allQuestions = parseResults.reduce((acc, cur) => {
     const resultQuestion = cur.matches.map((r, i) => {
       return {
         type: 'confirm',
-        name: cur.path.replace('.html', `:${i}`),
+        name: cur.path.replace('.html', `:${i}`), // NB: Dot ('.') will nest the result, avoid it in path.
         message: `
 Confirm change in ${cur.path}:
 
-- ${r.line}
-+ ${r.suggestion} \n \n`,
+${colors.red(`- ${r.line}`)}
+${colors.green(`+ ${r.suggestion}`)} \n \n`,
         default: true
       }
     })
@@ -31,13 +50,97 @@ Confirm change in ${cur.path}:
   }, [])
 
   const parseAnswers = (answers) => {
-    console.log(answers)
-    console.log(`✅  aaaaaand done. Updated all the things!`)
+    const mapAnswerIndexes = (flag) => {
+      // This method makes it possible to handle true / false values of answers[cur] (flag), without writing the same logic twice.
+      return (acc, cur) => {
+        const path = cur.substring(0, cur.lastIndexOf(':')) + '.html'
+        const index = parseInt(cur.substring(cur.lastIndexOf(':') + 1))
+        const valid = flag ? answers[cur] : !answers[cur]
 
-    // const suggestions = await replacer({
-    //   path,
-    //   lines: matches
-    // })
+        if (valid) {
+          if (!acc[path]) {
+            acc[path] = [index]
+          } else {
+            acc[path] = [...acc[path], index]
+          }
+        }
+
+        return acc
+      }
+    }
+
+   /**
+    * Workflow
+    * 1. Categorize answers.
+    * 2. Map categorized answers with their matches.
+    * 3. Make replaced content & output to file.
+    */
+    const denied = Object
+      .keys(answers)
+      .reduce(mapAnswerIndexes(false), {})
+
+    const accepted = Object
+      .keys(answers)
+      .reduce(mapAnswerIndexes(true), {})
+
+    const mapPathToAnswers = (subject) => {
+      return (item) => {
+        const matchList = subject[item]
+        const matches = parseResults
+          .find((m) => m.path === item)
+          .matches
+          .filter((m, i) => matchList.indexOf(i) !== -1)
+
+        return {
+          path: item,
+          matches
+        }
+      }
+    }
+
+    const deniedWithMatches = Object
+      .keys(denied)
+      .map(mapPathToAnswers(denied))
+
+    const acceptedWithMatches = Object
+      .keys(accepted)
+      .map(mapPathToAnswers(accepted))
+
+    return Promise
+      .all(
+        acceptedWithMatches
+          .map(async (item) => {
+            const fullPath = path.join(outputDir, item.path)
+            const content = await replacer({
+              path: item.path,
+              lines: item.matches
+            })
+
+            return outputer({ content, outputFile: fullPath})
+          })
+      )
+      .then(() => {
+        if (deniedWithMatches.length) {
+          console.log(`
+⚠️  Some lines were skipped, you might want to check them:
+--------------------------------------------------------
+          `)
+          deniedWithMatches.forEach((item) => {
+            console.log(`
+File: ${colors.green(item.path)}
+Lines:
+            `)
+
+            item.matches.map((m) => console.log(`- ${colors.yellow(m.line)}`))
+          })
+        }
+
+        console.log(`
+----------------------------------------
+✅  aaaaaand done.
+
+Replaced ${colors.blue(acceptedWithMatches.length)} occurences of ${colors.red(search)} with ${colors.green(replace)}.`)
+      })
   }
 
   return inquirer
@@ -45,24 +148,9 @@ Confirm change in ${cur.path}:
     .then(parseAnswers)
 }
 
-runtime()
-
-// {
-//   type: 'confirm',
-//     name: 'test-address/test-address:1',
-//       message: `
-// Confirm change in test-address/test-address.html:
-
-// - href="../polymer/polymer.html"
-// + href="../lib/polymer/polymer.html" \n \n`,
-//     default: true
-// }
-
-// const parseAnswers = (answers) => {
-//   console.log(answers)
-//   console.log(`✅  aaaaaand done. Updated all the things!`)
-// }
-//
-// inquirer
-//   .prompt(questions)
-//   .then(parseAnswers)
+try {
+    runtime()
+} catch (e) {
+    console.error(e)
+    console.error('Importer failed misserably ☹. ')
+}
